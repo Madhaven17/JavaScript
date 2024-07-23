0094e5a4e187b8fa98a4e7e87f59eeddd7877ab3
madhaven <madhavenmaddy17@gmail.com>
2024-07-23 18:14:14 +0530

    created a new project of api for git profile search in java Script.

--- javaScript/src/CreateNotes/Notes.js
+++ javaScript/src/CreateNotes/Notes.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const addBtn = document.getElementById('add');
     const notesContainer = document.getElementById('notes-container');
-
+    loadNotes();
     addBtn.addEventListener("click", () => {
         addNewNote();
     });
 
-    function addNewNote() {
+    function addNewNote(content = '') {
         const note = document.createElement("div");
         note.classList.add("note");
         note.innerHTML = `
@@ -15,8 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     <button class="edit"><i class="fa fa-paper-plane-o"></i></button>
                     <button class="delete"><i class="fa fa-trash-o"></i></button>
                 </div>
-                <div class="main hidden"></div>
-                <textarea></textarea>
+                <div class="main ${content ? '' : 'hidden'}">${content}</div>
+                <textarea class="${content ? 'hidden' : ''}"></textarea>
             </div>
         `;
 
@@ -24,6 +24,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const deleteBtn = note.querySelector('.delete');
         const main = note.querySelector('.main');
         const textArea = note.querySelector('textarea');
+
+        textArea.value = content;
 
         editBtn.addEventListener("click", () => {
             if (textArea.classList.contains('hidden')) {
@@ -34,13 +36,29 @@ document.addEventListener('DOMContentLoaded', () => {
                 main.classList.remove('hidden');
                 textArea.classList.add('hidden');
                 main.innerHTML = textArea.value;
+                saveNotes();
             }
         });
 
         deleteBtn.addEventListener("click", () => {
             note.remove();
+            saveNotes();
         });
 
         notesContainer.appendChild(note);
+    }
+
+    function saveNotes() {
+        const notes = [];
+        document.querySelectorAll('.note').forEach(note => {
+            const main = note.querySelector('.main');
+            notes.push(main.innerHTML);
+        });
+        localStorage.setItem('notes', JSON.stringify(notes));
+    }
+
+    function loadNotes() {
+        const notes = JSON.parse(localStorage.getItem('notes') || '[]');
+        notes.forEach(note => addNewNote(note));
     }
 });
